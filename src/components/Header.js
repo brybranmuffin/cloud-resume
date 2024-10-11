ec9f11c4574613278d2b763ac831a98430ab940c
brybranmuffin <41463209+brybranmuffin@users.noreply.github.com>
2024-10-11 13:00:05 -0700

    reverting header.js change

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,9 @@ export default function Header(){
     return (
     <div class="header">
         <ul class="header--pages">
-            <li><a href='cloud_resume/info'>Info</a></li>
+            <li><a href='/info'>Info</a></li>
             {/* <li><a href='/projects'>Projects</a></li> */}
-            <li><a href='cloud_resume/experience'>Experience</a></li>
+            <li><a href='/experience'>Experience</a></li>
         </ul>
     </div>
    
