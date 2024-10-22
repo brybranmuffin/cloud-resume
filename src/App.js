a92bf90a484898c7fb399876da8ba61321a545eb
brybranmuffin <41463209+brybranmuffin@users.noreply.github.com>
2024-10-21 21:10:00 -0500

    HashRouter implemented over BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Route, Routes, Link } from "react-router-dom";
+
 import './App.css';
 import Header from "./components/Header"
 import Footer from "./components/Footer"
@@ -11,14 +12,14 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-          <Route path="/cloud_resume/info" element={<Info />} />
-          <Route path="/cloud_resume/" element={<Info />} />
+          <Route path="/info" element={<Info />} />
+          <Route path="/" element={<Info />} />
           {/* <Route path="/projects" element={<Projects />} /> */}
-          <Route path="/cloud_resume/experience" element={<Experience />} />
+          <Route path="/experience" element={<Experience />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       {/* <Footer /> */}
 
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,15 @@
 import react from 'react'
 import reactDOM from 'react-dom'
+import { Link }  from "react-router-dom";
+
 
 export default function Header(){
     return (
     <div class="header">
         <ul class="header--pages">
-            <li><a href='/info'>Info</a></li>
+            <li><Link to='/info'>Info</Link></li>
             {/* <li><a href='/projects'>Projects</a></li> */}
-            <li><a href='/experience'>Experience</a></li>
+            <li><Link to='/experience'>Experience</Link></li>
         </ul>
     </div>
    
